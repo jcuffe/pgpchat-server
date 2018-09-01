e31d4b1775f3d845ea1f3f501bfbbe3670cf068f
Jared Cuffe <jcuffe@gmail.com>
2018-09-01 11:10:45 -0700

    File structure reorg.

--- index.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const graphqlHandler = require('./schema.js');
-
-const server = express();
-const port = process.env.PORT || 8000;
-const announce = () => console.log(`Listening on ${port}`);
-
-server.use('/graphql', graphqlHandler);
-server.listen(port, announce);
